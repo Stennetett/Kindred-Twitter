a1745353f3b649ab484ed7002a06970f42f14c33
Stennetett <marcus95a@gmail.com>
2019-11-12 18:37:33 +0100

    Added files

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Hello from "./mainMenu"
+
+function About() {
+  return (
+ <div>
+<h1>About Page</h1>
+ </div>
+  );
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./mainMenu";
+import Navigation from "./Navigation";
+import About from "./About";
+import Shop from "./Shop";
+
+
+function App() {
+  return (
+    <div>
+      <Hello />
+
+      
+
+    </div>
+  );
+}
+
+export default App;
