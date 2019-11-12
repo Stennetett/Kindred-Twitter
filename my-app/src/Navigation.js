5bbbcb535172f9055bd5d01da15050d1a9e7be4a
Stennetett <56960738+Stennetett@users.noreply.github.com>
2019-11-12 23:05:25 +0100

    Changed color for search bar

---
+++ my-app/src/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Hello from "./mainMenu"
+
+function Navigation() {
+  return (
+ <nav>
+<ul className = "nav-links">
+    <li>About</li>
+    <li>Shop</li>
+</ul>
+ </nav>
+  );
+}
+
+export default Navigation;
---
+++ my-app/src/mainMenu.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Navigation from "./Navigation";
+import About from "./About";
+import Shop from "./Shop";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function Hello() {
+
+    return (
+
+        <Router>
+            <div>
+
+                <div id="test">
+               
+
+                    <div id="iconText">
+                        <img class="bottom" src={require('./kindredtwittertext.png')} />
+
+
+                    </div>
+                    <div id="birdImage">
+                        <img class="top" src={require('./twitterlogogreen.png')} />
+
+                    </div>
+
+
+                   
+
+
+
+                </div>
+
+
+                <div id="wrapper">
+                    <div className="popup">
+                        
+                    <input className="text" type="text" placeholder="Search.."></input>
+
+                    <div id="searchIcon">
+                        <img className="search" color = "green" src={require('./searchicon.png')} />
+
+                    </div>
+
+                      
+                        
+
+
+                    </div>
+                 
+                </div>
+                <div className = "longSide">
+                        
+                </div>
+
+
+
+
+
+
+                <Switch>
+                    <Route path="/about" component={About} />
+                    <Route path="/shop" component={Shop} />
+                </Switch>
+
+
+
+            </div>
+        </Router >
+
+    )
+}
+
+export default Hello;
